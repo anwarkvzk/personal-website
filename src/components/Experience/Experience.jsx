ab073b3f302c5e8223fe12b5de0c103fca91ad8e
anwarkvzk <anvarvzk521@gmail.com>
2022-09-29 20:10:21 +0530

    Experience Component Styled

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -13,29 +13,39 @@ function Experience() {
           <h3>Frontend Development</h3>
           <div className="experience__content">
             <article className="experience__details">
-              <BsPatchCheckFill />
+              <BsPatchCheckFill className="experience__details-icon"/>
+              <div>
               <h4>HTML 5</h4>
               <small className="text-light">Experinced</small>
+              </div>
             </article>
             <article className="experience__details">
-              <BsPatchCheckFill />
+              <BsPatchCheckFill className="experience__details-icon"/>
+              <div>
               <h4>CSS</h4>
               <small className="text-light">Intermediate</small>
+              </div>
             </article>
             <article className="experience__details">
-              <BsPatchCheckFill />
+              <BsPatchCheckFill className="experience__details-icon"/>
+              <div>
               <h4>JavaScript</h4>
               <small className="text-light">Intermediate</small>
+              </div>
             </article>
             <article className="experience__details">
-              <BsPatchCheckFill />
+              <BsPatchCheckFill className="experience__details-icon"/>
+              <div>
               <h4> BootStrap</h4>
               <small className="text-light">Experinced</small>
+              </div>
             </article>
             <article className="experience__details">
-              <BsPatchCheckFill />
+              <BsPatchCheckFill className="experience__details-icon"/>
+              <div>
               <h4>React</h4>
               <small className="text-light">Experinced</small>
+              </div>
             </article>
           </div>
         </div>
@@ -43,24 +53,32 @@ function Experience() {
           <h3>Backend Development</h3>
           <div className="experience__content">
             <article className="experience__details">
-              <BsPatchCheckFill />
+              <BsPatchCheckFill className="experience__details-icon"/>
+              <div>
               <h4>Node JS</h4>
               <small className="text-light">Intermediate</small>
+              </div>
             </article>
             <article className="experience__details">
-              <BsPatchCheckFill />
+              <BsPatchCheckFill className="experience__details-icon"/>
+              <div>
               <h4>Python 3</h4>
               <small className="text-light">Intermediate</small>
+              </div>
             </article>
             <article className="experience__details">
-              <BsPatchCheckFill />
+              <BsPatchCheckFill className="experience__details-icon"/>
+              <div>
               <h4> MongoDB</h4>
               <small className="text-light">Experienced</small>
+              </div>
             </article>
             <article className="experience__details">
-              <BsPatchCheckFill />
+              <BsPatchCheckFill className="experience__details-icon"/>
+              <div>
               <h4> PostMan</h4>
               <small className="text-light">Experienced</small>
+              </div>
             </article>
           </div>
         </div>
