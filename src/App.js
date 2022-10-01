dac1623c11e2c4023be1e7efcef08566de8bba07
anwarkvzk <anvarvzk521@gmail.com>
2022-10-01 09:28:51 +0530

    Nav Updated

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from "./components/header/Header";
 import Nav from "./components/nav/Nav";
 import About from "./components/about/About";
 import Experience from './components/Experience/Experience'
-import Services from './components/services/Services'
+// import Services from './components/services/Services'
 import Portfolio from './components/portfolio/Portfolio'
 import Contact from './components/Contact/Contact'
 import Footer from './components/footer/Footer'
@@ -15,7 +15,7 @@ function App() {
       <Nav />
       <About />
       <Experience />
-      <Services />
+      {/* <Services /> */}
       <Portfolio />
       <Contact />
       <Footer />
